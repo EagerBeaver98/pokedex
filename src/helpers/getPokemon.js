75a95b899afc02568550b112738c519aa65687b5
John <jooke98@outlook.com>
2021-04-15 15:32:21 -0600

    games are passed with generation info

--- src/helpers/getPokemon.js
+++ src/helpers/getPokemon.js
@@ -11,66 +11,97 @@ const makeRequestsFromObject = (versions) => {
   return finalRequests;
 };
 
-const getPokedexFlavorText = (pokemon, version, callback) => {
-  axios.get("https://pokeapi.co/api/v2/pokemon-species/" + pokemon)
-  .then((poke) => {
-    for (let pokedex of poke.data.flavor_text_entries) {
-      if (pokedex.version.name === version && pokedex.language.name === 'en') {
-        callback(pokedex);
+const getGames = async (generation, callback) => {
+  const versionGroups = [];
+  const games = [];
+  for (let version_group of generation.version_groups) {
+    versionGroups.push(axios.get(version_group.url))
+  }
+  await axios.all(versionGroups).then(axios.spread((...groups) => {
+    for (let group of groups) {
+      for (let game of group.data.versions) {
+        games.push(game);
       }
     }
-  })
+    callback(games)
+  }))
   .catch((err) => {
     console.log(err);
-  })
+  });
 };
+
+// const getPokedexFlavorText = (pokemon, version, callback) => {
+//   axios.get("https://pokeapi.co/api/v2/pokemon-species/" + pokemon)
+//   .then((poke) => {
+//     for (let pokedex of poke.data.flavor_text_entries) {
+//       if (pokedex.version.name === version && pokedex.language.name === 'en') {
+//         callback(pokedex);
+//       }
+//     }
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   })
+// };
 
 const getPokemon = (pokemon, callback) => {
   const pokemonData = axios.get("https://pokeapi.co/api/v2/pokemon/" + pokemon);
   const pokemonSpeciesData = axios.get("https://pokeapi.co/api/v2/pokemon-species/" + pokemon);
+  let combinedResponses = {};
+  const fullVersion = [];
   axios.all([pokemonData, pokemonSpeciesData])
     .then(axios.spread( async (...responses) => {
-      let combinedResponses = {...responses[0].data, species: responses[1].data, name: nameFormat(responses[0].data.name)};
+      combinedResponses = {...responses[0].data, species: responses[1].data, name: nameFormat(responses[0].data.name)};
       const versions = makeRequestsFromObject(responses[0].data.sprites.versions)
-      const fullVersion = [];
-      await axios.all(versions).then(axios.spread((...genResponses) => {
+      await axios.all(versions).then(axios.spread( async (...genResponses) => {
         for (let response of genResponses) {
           fullVersion.push(response.data);
         }
-        combinedResponses = {...combinedResponses.species, versions: fullVersion.reverse()}
-        callback(combinedResponses)
+        
+        for (let x = 0; x < Object.keys(fullVersion).length; x++) {
+          await getGames(fullVersion[x], (games) => {
+            fullVersion[x] = {...fullVersion[x], games};
+          })
+        }
+        combinedResponses = {...combinedResponses.species, generations: fullVersion};
         }))
+        .then(() => {
+          callback(combinedResponses)
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     }))
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
     });
 };
 
-const getGameList = (gen, callback) => {
-  let results = [];
-  axios.get("https://pokeapi.co/api/v2/generation/" + gen)
-  .then( async ({ data }) => {
-    for (let group of data.version_groups) {
-      await axios.get(group.url)
-      .then((response) => {
-        response.data.versions.map((version) => results.push({...version, name: nameFormat(version.name)}))
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-    }
-  })
-  .catch((err) => {
-    console.log(err);
-  })
-  .finally(() => {
-    callback(results);
-  })
-};
+// const getGameList = (gen, callback) => {
+//   let results = [];
+//   axios.get("https://pokeapi.co/api/v2/generation/" + gen)
+//   .then( async ({ data }) => {
+//     for (let group of data.version_groups) {
+//       await axios.get(group.url)
+//       .then((response) => {
+//         response.data.versions.map((version) => results.push({...version, name: nameFormat(version.name)}))
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       })
+//     }
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   })
+//   .finally(() => {
+//     callback(results);
+//   })
+// };
 
 // makeRequestsFromArray([1])
 // getGameList(1, console.log)
-getPokemon(1, console.log)
+getPokemon(2, console.log)
 // getPokemonSpecies(1, console.log)
 
-module.exports = { getPokemon, getPokedexFlavorText, getGameList };
+module.exports = { getPokemon };
